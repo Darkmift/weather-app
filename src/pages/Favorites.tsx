a1cf6d070978d34643d8a2d1290404331d05d917
Darkmift <darkmift@gmail.com>
2024-01-06 01:07:00 +0200

    [rtk-query] Refactor Favorites page to use MUI Container

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,5 +1,7 @@
+import { Container } from '@mui/material';
+
 const Favorites = () => {
-  return <div>Favorites</div>;
+  return <Container sx={{ display: 'flex', flexDirection: 'column', gap: 5 }}>Favorites</Container>;
 };
 
 export default Favorites;
